3c766e593d9f0ac3ba9efdc4c159fb2c6537ff9a
Sosnov VA <vlad@youteam.co.uk>
2021-01-02 20:45:32 +0200

    initial settings

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about-view">
+    about-view
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AboutView'
+})
+</script>
