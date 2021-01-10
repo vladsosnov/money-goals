0623cfad1e05e1f69814f0d9934038df5627afa6
vladsosnov <vladsosnov2001@gmail.com>
2021-01-10 14:40:40 +0200

    fix header gradient

--- src/components/Shared/AppHeader.vue
+++ src/components/Shared/AppHeader.vue
@@ -1,57 +1,59 @@
 <template>
-  <header class="app-header container py-3 px-2 sm:px-4 flex justify-between items-center">
-    <router-link :to="{ name: 'Index' }">
-      <img
-        src="@/assets/images/shared/logo.png"
-        alt="Site Logo"
-        class="app-header__logo"
-      >
-    </router-link>
-    <nav>
-      <ul class="flex items-center space-x-4 sm:space-x-8">
-        <li>
-          <router-link
-            exact
-            :to="{ name: 'Index' }"
-            class="text-alto hover:text-gray-slate font-medium"
-            active-class="font-bold"
-          >
-            Main
-          </router-link>
-        </li>
-        <li>
-          <router-link
-            :to="{ name: 'Goals' }"
-            class="text-alto hover:text-gray-slate font-medium"
-            active-class="font-bold"
-          >
-            Goals
-          </router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: 'Login' }">
-            <img
-              src="@/assets/images/shared/account.svg"
-              alt="Account"
+  <header class="app-header vs-gradient">
+    <div class="container py-3 px-2 sm:px-4 flex justify-between items-center">
+      <router-link :to="{ name: 'Index' }">
+        <img
+          src="@/assets/images/shared/logo.png"
+          alt="Site Logo"
+          class="app-header__logo"
+        >
+      </router-link>
+      <nav>
+        <ul class="flex items-center space-x-4 sm:space-x-8">
+          <li>
+            <router-link
+              exact
+              :to="{ name: 'Index' }"
+              class="text-alto hover:text-gray-slate font-medium"
+              active-class="font-bold"
             >
-          </router-link>
-        </li>
-        <li v-if="isAuthorized">
-          <router-link
-            :to="{ name: 'Login' }"
-            class="
-              py-3 sm:py-4 px-4 sm:px-10
-              flex justify-center items-center
-              text-white text-base sm:text-xl font-semibold
-              bg-green hover:bg-green-active rounded-10
-              transition duration-200 ease-in
-            "
-          >
-            Join now
-          </router-link>
-        </li>
-      </ul>
-    </nav>
+              Main
+            </router-link>
+          </li>
+          <li>
+            <router-link
+              :to="{ name: 'Goals' }"
+              class="text-alto hover:text-gray-slate font-medium"
+              active-class="font-bold"
+            >
+              Goals
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'Login' }">
+              <img
+                src="@/assets/images/shared/account.svg"
+                alt="Account"
+              >
+            </router-link>
+          </li>
+          <li v-if="isAuthorized">
+            <router-link
+              :to="{ name: 'Login' }"
+              class="
+                py-3 sm:py-4 px-4 sm:px-10
+                flex justify-center items-center
+                text-white text-base sm:text-xl font-semibold
+                bg-green hover:bg-green-active rounded-10
+                transition duration-200 ease-in
+              "
+            >
+              Join now
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
